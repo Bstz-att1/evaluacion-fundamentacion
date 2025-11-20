ef496d71c3609358370b48e4f6be3f6336e1e20a
Bstz-att1 <hfdarioalexander7@gmail.com>
2025-11-19 20:24:50 -0500

    Arreglo punto 5 y 4

--- app4.js
+++ app4.js
@@ -3,10 +3,7 @@ import {generarTabla} from "./modulos4/index4.js";
 function solicitarTabla() {
     const numero = parseInt(prompt("Ingrese el número para la tabla de multiplicar:"));
     const tabla = generarTabla(numero);
-    console.log(`Tabla de multiplicar del ${numero}:`);
-    tabla.forEach((resultado, i) => {
-        console.log(`${numero} x ${i + 1} = ${resultado}`);
-    });
+    console.log(`Tabla de multiplicar del ${numero}:`, tabla);
 }
 
 solicitarTabla();
--- modulos6/existenciaelemento.js
+++ modulos6/existenciaelemento.js
@@ -1,11 +1,11 @@
 const buscarElemento = (lista, elemento) => {
     for (let i = 0; i < lista.length; i++) {
         if (lista[i] === elemento) {
-            console.log(`El elemento "${elemento}" fue encontrado en la lista.`);
+            console.log(`El elemento "${elemento}" si fue encontrado en la lista, ya puedes realizar la compra del elemento.`);
             return true;
         }
     }
-    console.log(`El elemento "${elemento}" no fue encontrado en la lista.`);
+    console.log(`Lo lamentamos. El elemento "${elemento}" no fue encontrado en la lista, por favor busca otro articulo.`);
     return false;
 };
 
